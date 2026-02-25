8e92059492ffe8a8a93050f756dcb759e3389712
EdklauseDiniz <edklausezinho@gmail.com>
2026-02-25 12:47:17 -0300

    Add files via upload

--- myproject/App.js
+++ myproject/App.js
@@ -1,53 +1,114 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { StyleSheet, Text, View, ScrollView, SafeAreaView, navigation, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, TextInput, Image, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useRoute } from '@react-navigation/native';
 
-function HomeScreen({navigation, route}) {
+const Stack = createNativeStackNavigator();
 
-const { username, idade } = route.params;
+function HomeScreen({ navigation, route }) {
 
-return (
-<View style={{ flex: 1, alignItems: 'center'
+  const username = route?.params?.username;
+  const idade = route?.params?.idade;
 
-, justifyContent: 'center' }}>
-
-<Text>Bem-vindo, {username}!</Text>
-<Text>Idade: {idade}</Text>
-</View>
-);
+  return (
+    <SafeAreaView style={{flex:1}}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Marcos Andrade</Text>
+        <Text>81 99614-2890</Text>
+        <Text>Idade: {idade}</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
 
-function LoginScreen({navigatio}) {
+function LoginScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>LoginScreen</Text>
-      <Button title='Home' onPress={() => navigation.navigate('Home', {username:'Nilson',idade:30})} />
+      <Image source={{
+        uri:'https://cdn-icons-png.flaticon.com/512/3135/3135715.png'}} 
+        style={styles.login}/>
+      <Text style={styles.text}>Login</Text>
+      <TextInput style={styles.input}></TextInput>
+      <Text style={styles.text}>Senha</Text>
+      <TextInput style={styles.input}></TextInput>
+      <TouchableOpacity
+        style={styles.buttonazul}
+        onPress={() => navigation.navigate('Home', { username: 'Nilson', idade: 30 })}
+      >
+        <Text style={{ color: '#fff' }}>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.buttonvermelho}
+        onPress={() => navigation.navigate('Home', { username: 'Nilson', idade: 30 })}
+      >
+        <Text style={{ color: '#fff'}}>Cadastrar-se</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-function App() {
+export default function App() {
   return(
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerRight: () => (
+          <TouchableOpacity>
+            <Image style={styles.add} source={{uri:'https://cdn-icons-png.flaticon.com/512/54/54414.png'}}/>
+          </TouchableOpacity>
+        )}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-export default App;
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fdc761',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  login: {
+    padding: 60,
+    margin: 50,
+  },
+
+  add: {
+    padding: 15,
+    right: 75,
+  },
+
+  text: {
+    fontSize: 40,
+  },
+
+  input: {
+    backgroundColor: '#fff',
+
+    borderColor: '#000000',
+    borderRadius: 5,
+    borderWidth: 1,
+
+    margin: 15,
+    height: 20,
+    width: 150,
+  },
+
+  buttonazul: {
+    marginTop: 20,
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+
+  buttonvermelho: {
+    marginTop: 20,
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 5,
+  }
 });
